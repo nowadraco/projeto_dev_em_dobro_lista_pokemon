be8143819309c0f82b773c61bad409c45ec2422e
nowadraco <Nowadraco@gmail.com>
2023-05-14 00:41:57 +0000

    projeto finalizado

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaotrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscurEstaAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscurEstaAtivo) {
+        body.classList.remove("modo-escuro");
+        imagemBotaotrocaDeTema.setAttribute("src", "/src/imagens/sun.png");
+    } else {
+        body.classList.add("modo-escuro");
+        imagemBotaotrocaDeTema.setAttribute("src", "/src/imagens/moon.png");
+    }
+});
